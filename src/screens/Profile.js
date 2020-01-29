04cd62592104756f772a6cb0982599a1469bc555
zidniryi <mac@macs-MacBook-Air.local>
2020-01-30 00:48:20 +0700

    make basic user profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import {
+    Text,
+    View,
+    Image,
+    TouchableOpacity
+} from 'react-native'
+import styles from '../styles/styleProfile'
+import Axios from 'axios'
+
+
 
 export default class Profile extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: false
+        }
+    }
+
+    componentDidMount() {
+        Axios.get('https://api.github.com/users/zidniryi')
+            .then((response) => {
+                console.log(response)
+                this.setState({ isLoading: false })
+            })
+            .catch(err => {
+                this.setState({ isLoading: false })
+                console.log(err)
+            })
+
+    }
+
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={styles.container}>
+                <View style={styles.header}></View>
+                <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
+                <View style={styles.body}>
+                    <View style={styles.bodyContent}>
+                        <Text style={styles.name}>John Doe</Text>
+                        <Text style={styles.info}>UX Designer / Mobile developer</Text>
+                        <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
+                        <TouchableOpacity style={styles.buttonContainer}>
+                            <Text style={styles.textSite}>See Me</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         )
     }
 }
+
+
---
+++ src/styles/styleProfile.js
@@ -0,0 +1,61 @@
+import { StyleSheet } from 'react-native'
+import { responsiveHeight, responsiveFontSize } from 'react-native-responsive-dimensions'
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#53AD15',
+        height: responsiveHeight(28)
+    },
+    avatar: {
+        width: 130,
+        height: 130,
+        borderRadius: 63,
+        borderWidth: 4,
+        borderColor: 'white',
+        marginBottom: 10,
+        alignSelf: 'center',
+        position: 'absolute',
+        marginTop: 130
+    },
+    body: {
+        marginTop: responsiveHeight(8),
+    },
+    bodyContent: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 30,
+    },
+    name: {
+        fontSize: responsiveFontSize(5),
+        color: '#696969',
+        fontWeight: '600'
+    },
+    info: {
+        fontSize: 16,
+        color: '#00BFFF',
+        marginTop: 10
+    },
+    description: {
+        fontSize: 16,
+        color: '#696969',
+        marginTop: 10,
+        textAlign: 'center'
+    },
+    buttonContainer: {
+        marginTop: 10,
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 250,
+        borderRadius: 30,
+        backgroundColor: '#00BFFF',
+    },
+    textSite: {
+        color: '#FFFFFF',
+        fontSize: responsiveFontSize(2)
+    }
+})
+
+export default styles
